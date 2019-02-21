7b2e785822cf23eb4d100a566f3a0ed194f3f29e
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-02-21 13:38:30 -0800

    Completed Functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,16 +6,19 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // stores data into storage
     storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    // subtracts data from storage
     var popped = storage[Object.keys(storage).length - 1];
     delete storage[Object.keys(storage).length - 1];
     return popped;
   };
 
   someInstance.size = function() {
+    //get the length of contained data
     return Object.keys(storage).length;
   };
 
