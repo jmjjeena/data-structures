023ffc7cbead9e99534cf02b9ae5f1cc2c40f8b4
John J Nichols <johnjaynichols@gmail.com>
2019-02-20 22:04:35 -0800

    Implement enqueue & dequeue functions.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,31 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var count = 0;
 
   someInstance.enqueue = function(value) {
+  //add value to back of queue
+  storage[count] = value;
+  count++;
   };
 
   someInstance.dequeue = function() {
+  //remove value from front of queue
+  for (var key in storage) {
+    delete storage[key];
+      break; 
+    }
   };
 
   someInstance.size = function() {
+  //length of queue
+  var keyCount = 0;
+    for (var key in storage) {
+     if (storage[key]) {
+      keyCount++;
+      }
+    }
+    return keyCount;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,27 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  // {1: value} - > after push - >
+  // {2: value, 1: value}
+  // - > after pop - > {1: value}
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    delete storage[count];
+    count--;
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    }
+    return count;
   };
 
   return someInstance;
