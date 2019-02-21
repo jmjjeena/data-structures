dc906345426b65a941f22d158a5e7753de7b47c5
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-02-21 13:37:21 -0800

    Completed Functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // stores data into storage
+      storage[count] = value;
+        count++;
+    
   };
 
   someInstance.dequeue = function() {
+    // subtracts data from storage
+   var first = Object.keys(storage)[0];
+   var popped = storage[first];
+   delete storage[first];
+   return popped;
+   if(count !== 0) {count --;}
+ 
   };
 
   someInstance.size = function() {
+    //get the length of contained data
+    return Object.keys(storage).length;
   };
 
   return someInstance;
