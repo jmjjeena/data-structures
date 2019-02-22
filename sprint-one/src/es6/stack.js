b2f5d7f4b5630933b42b6fbc18f131e98737fa20
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-22 01:08:17 -0800

    Completed es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.storage = {};
   }
+
+  push(value) {
+	  this.storage[this.count] = value;
+	  this.count++;
+  };
+
+  pop() {
+	  var removed = this.storage[this.count - 1];
+	  delete this.storage[this.count - 1];
+	  if (this.count > 0) {
+	  	this.count--;
+	  };
+	  return removed; 
+	};
+
+	size() {
+    //get the length of contained data
+    return this.count;
+	};
 
 }
