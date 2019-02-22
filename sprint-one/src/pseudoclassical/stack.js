3c646970959ba80d87eba509e273687f2ba0615a
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-22 00:53:56 -0800

    Complete pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+
+Stack.prototype.push = function (value) {
+  this.storage[this.count] = value;
+  this.count++;
+}
+
+Stack.prototype.pop = function() {
+  var removed = this.storage[this.count - 1];
+  delete this.storage[this.count - 1];
+  if (this.count > 0) {
+  	this.count--;
+  }
+  return removed; 
+}
+
+Stack.prototype.size = function() {
+    //get the length of contained data
+    return this.count;
+};
 
