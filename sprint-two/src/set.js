f50eb16b8306cfac83a828acea31f2fffa592182
jmjjeena <jmj.jeena@gmail.com>
2019-02-23 20:15:11 -0800

    Implement set solution.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
+  set.counter = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[this.counter] = item;
+  
+  this.counter++;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage)   {
+    if(this._storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  
+  for (var i in this._storage){
+    if (this._storage[i] === item){
+      delete this._storage[i];
+      this.conter--;
+    }
+  }
+
 };
 
 /*
