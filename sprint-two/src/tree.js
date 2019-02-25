a302ac9b518007cb82dd45a66287380c5f12ce7e
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-25 07:37:05 -0800

    Implement tree solutions & time complexity

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
-var newTree = {};
-newTree.value = value;
+  var newTree = {};
+  newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];
   _.extend(newTree, treeMethods);
   return newTree;
 };
@@ -18,46 +18,24 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  //take target input
-  //search data structure for target
-  //return true if found
-  //return false else
-  //
-  //search main node 
-  //if not found, enter recursive function
-  //for loop through children 
-  //if not found, for loop through their children
-  //else return false
   if (this.value === target) {
     return true;
   }
-    // var searchTree = function(node){
-      for(var i = 0; i < this.children.length; i++) {
-        if (this.children[i].value === target) {
-                return true;
-        }
-        if (this.children[i].contains(target)) {
-          return true;
-        }
-      }
-    // }
-    
-    return false;
-  
-
-  // var searchTree = function(node) {
-  //   for (var i = 0; i < node.length; i++) {
-  //     if (node[i].value.contains(target)) {
-  //       return true;
-  //     }
-  //   }
-  //   searchTree(this.children);
-  //   return false;
-  // }                 
+  // var searchTree = function(node){
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ * For addChild(), the time complexity is Linear O(n).
+ * For contains(), the time complexity is Linear O(n).
  */
