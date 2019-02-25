100e03fc349de5befeae5c8f2df4d789ffbcf8e5
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-25 07:39:02 -0800

    Implement set solution & time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -9,13 +9,12 @@ var setPrototype = {};
 
 setPrototype.add = function(item) {
   this._storage[this.counter] = item;
-  
   this.counter++;
 };
 
 setPrototype.contains = function(item) {
-  for (var key in this._storage)   {
-    if(this._storage[key] === item) {
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
       return true;
     }
   }
@@ -23,16 +22,18 @@ setPrototype.contains = function(item) {
 };
 
 setPrototype.remove = function(item) {
-  
-  for (var i in this._storage){
-    if (this._storage[i] === item){
+  for (var i in this._storage) {
+    if (this._storage[i] === item) {
       delete this._storage[i];
-      this.conter--;
+      this.counter--;
     }
   }
-
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ * For add(), the time complexity is Linear O(n).
+ * For remove(), the time complexity is Linear O(n).
+ * For contains(), the time complexity is Linear O(n).
  */
