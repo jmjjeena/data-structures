c0c7fa13be5b5e53d74ece1f1b38c787c9962743
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-02-25 07:48:59 -0800

    Fix merge conflicts

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -5,27 +5,6 @@ var Stack = function() {
   this.storage = {};
 };
 
-Stack.prototype.push = function(value) {
-  this.count++;
-  this.storage[this.count] = value;
-  return this.storage[this.count];
-};
-
-Stack.prototype.pop = function() {
-  var toPop = this.storage[this.count];
-  delete this.storage[this.count];
-  this.count--;
-  return toPop;
-};
-
-Stack.prototype.size = function() {
-  if (this.count < 0) {
-    return 0;
-  } else {
-    return this.count;
-  }
-};
-
 Stack.prototype.push = function (value) {
   this.storage[this.count] = value;
   this.count++;
